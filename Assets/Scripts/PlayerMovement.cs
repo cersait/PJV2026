88886e3556f21d3f2a4fa3499a6c2e4fd3f2c94e
Aiden <aiden.chandran@elev.ga.lbs.se>
2026-03-02 11:15:06 +0100

    PlayerMove 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5f; // 
+    public float jumpForce = 5f; // 
+    public Transform groundCheck; // 
+    public float groundCheckRadius = 0.2f; // 
+    public LayerMask groundLayer; // 
+    private bool isFacingRight = true; 
+    private bool isGrounded = false; 
+    private Rigidbody2D rb; 
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // H�mta v�r Rigidbody 2D
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); // Kolla om spelaren �r p� marken
+        float moveDirection = Input.GetAxis("Horizontal");  // Kolla om vi r�r oss horisontellt
+        Move(moveDirection); // Flytta spelaren
+        if (moveDirection > 0 && !isFacingRight) // Flippa spelaren s� den tittar i den riktningen som den r�r sig i
+        {
+            Flip();
+        }
+        else if (moveDirection < 0 && isFacingRight) // Flippa spelaren s� den tittar i den riktningen som den r�r sig i
+        {
+            Flip();
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded) // Om vi tycker p� space, s� l�t spelaren hoppa
+        {
+            
+            Jump();
+        }
+    }
+    private void Move(float direction)
+    {
+        Vector2 movement = new Vector2(direction * moveSpeed, rb.linearVelocity.y); // r�kna ut hur vi r�r oss i relation till spelarens velocity
+        rb.linearVelocity = movement;
+    }
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse); // L�gg till vertical force f�r att kunna hoppa
+    }
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;  // Flippa player spriten horisontellt
+        transform.Rotate(0f, 180f, 0f);
     }
 }
+
