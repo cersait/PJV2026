5562499fccb0070106876d20e862f5a7f21c751a
Aiden <aiden.chandran@elev.ga.lbs.se>
2026-03-03 11:30:00 +0100

    hngrdhg8yhr
    mat

---
+++ Assets/Scripts/Talk text.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+using Unity.Collections;
+using System.Collections;
+using UnityEngine.UI;
+public class Talktext : MonoBehaviour
+{
+    public TextMeshProUGUI tmp;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tmp.text = string.Empty;
+        startText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void startText()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in lines[index].ToCharArray())
+        {
+            tmp.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+    }
+
+
+
+
+}
