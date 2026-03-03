9ed42830e6a45f3c1cbefd4c9373e1fb870114b6
Aiden <aiden.chandran@elev.ga.lbs.se>
2026-03-03 14:50:45 +0100

    Yo momma anyton

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    [SerializeField] private GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit"); // only works in build
+    }
+}
