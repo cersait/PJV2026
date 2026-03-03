355f79b3695353ac267cdf51e17403412cb85ecc
cersait <atillaesen162@gmail.com>
2026-03-03 10:28:09 +0100

    sceneTP

---
+++ Assets/Atilla Stuff/newSceneTP.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class newSceneTP : MonoBehaviour //made by Atilla
+{
+    public GameObject thePlayer; // själva playern
+    private void OnTriggerEnter2D(Collider2D other) //Om vi triggar en kollision byter playern position till Teleporter 2
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Goof scene");
+        }
+    }
+
+}
+
