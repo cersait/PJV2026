cf690876dce4dee9499c219955170fe78e6e0786
Aiden <aiden.chandran@elev.ga.lbs.se>
2026-03-05 13:50:39 +0100

    doors be dorrings why did the Spelare grafik leave us???????

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 using static Interfaces;
 
-public class Door : MonoBehaviour, IInteractable 
+public class Door : MonoBehaviour, IInteractable
 {
-    public string requiredKeyID;  // Example: "RedKey"
+    [SerializeField] private string requiredKeyID;  // Example: "RedKey"
+    [SerializeField] private Transform teleportPoint;
+    [SerializeField] private GameObject door;
+
+    private bool isUnlocked = false;
 
     public void Interact(GameObject interactor)
     {
@@ -25,4 +29,17 @@ public class Door : MonoBehaviour, IInteractable
         Debug.Log("Door opened!");
         Destroy(gameObject);
     }
+    public void UnlockDoor()
+    {
+        door.SetActive(false);
+    }
+
+  
+    void teleportPlayer(GameObject player)
+    {
+        player.transform.position = teleportPoint.position;
+
+    }
+
+
 }
