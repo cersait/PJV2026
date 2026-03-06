e8894077a5b1bd407dcf0b46997fb85521909e55
Aiden <aiden.chandran@elev.ga.lbs.se>
2026-03-06 11:16:55 +0100

    scale

--- Assets/Scripts/PlayerSceneScaler.cs
+++ Assets/Scripts/PlayerSceneScaler.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerSceneScaler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnEnable()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
-        
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Anton Scene")
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (scene.name == "Korridor")
+        {
+            transform.localScale = new Vector3(44.46503f, 9.338171f, 1f);
+        }
     }
 }
