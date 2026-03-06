2ddb1bfb10da8288142564b8cff206ab6ad3cb97
Aiden <aiden.chandran@elev.ga.lbs.se>
2026-03-06 12:28:43 +0100

    afsf

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -2,18 +2,6 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    
-    void Start()
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-        if (player != null)
-        {
-            player.transform.position = transform.position;
-        }
-
-
-    }
 
    
 }
